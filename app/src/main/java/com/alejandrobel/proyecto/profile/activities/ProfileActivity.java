da9ff09889da02e77ecfd37065c2145a0109919b
OverKira99 <alejobel1000@gmail.com>
2025-05-27 15:18:13 -0500

    funcional

--- app/src/main/java/com/alejandrobel/proyecto/profile/activities/ProfileActivity.java
+++ app/src/main/java/com/alejandrobel/proyecto/profile/activities/ProfileActivity.java
@@ -1,26 +1,49 @@
 package com.alejandrobel.proyecto.profile.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.alejandrobel.proyecto.R;
+import com.alejandrobel.proyecto.auth.activities.LoginActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    Button btnlogut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnlogut = findViewById(R.id.btn_logout);
+        toolbar = findViewById(R.id.top_app_bar_profile);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnlogut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
         });
     }
 }
